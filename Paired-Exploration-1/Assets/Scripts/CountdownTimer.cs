a5beef5176e979fe23c30fe344fad7aafc3438a8
JamesL404 <92769795+JamesL404@users.noreply.github.com>
2023-06-06 13:12:51 -0700

    Player/Timer

---
+++ Paired-Exploration-1/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private float currentTime = 0.0f;
+    private float startingTime = 60.0f;
+
+    [SerializeField] private TMP_Text countdownText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        int roundTime = (int)currentTime;
+        countdownText.text = roundTime.ToString();
+    }
+}
---
+++ Paired-Exploration-1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    [SerializeField] Rigidbody rb;
+
+    private float horizontalInput;
+    [SerializeField] float horizontalMultiplier = 2;
+
+    [SerializeField] float jumpForce = 600f;
+    [SerializeField] LayerMask groundMask;
+    private float speedMax = 100f;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (speed < speedMax)
+        {
+            speed += 1f;
+        }
+        Vector3 moveForward = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + moveForward + horizontalMove);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+    }
+
+    void Jump()
+    {
+        //Check if the player is on the ground
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height/2)+0.1f, groundMask);
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
